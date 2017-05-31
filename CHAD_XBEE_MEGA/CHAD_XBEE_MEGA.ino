8ea6f1248c8bd7ac0c35ccd4024cd4320e3ceeee
Jordan <diers033@umn.edu>
2017-05-31 17:05:07 -0500

    Putting stuff together

--- CHAD_XBEE_MEGA/CHAD_XBEE_MEGA.ino
+++ CHAD_XBEE_MEGA/CHAD_XBEE_MEGA.ino
@@ -3,7 +3,7 @@
 // students in the Physics 295 Independent Research course at
 // Wright College
 // Instructions and information found at http://physi.cz/chad
-//HI GUYS
+
 
 // Adafruit Motorshield v2
 #include <AccelStepper.h>
@@ -12,7 +12,13 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <SPI.h>
 #include <SD.h>
+#include <Adafruit_GPS.h>
 
+Adafruit_GPS GPS(&Serial2); //Constructor for GPS object
+
+File compassfile;
+File gpsfile;
+unsigned long flightStart = 0;
 
 Adafruit_MotorShield AFMStop(0x60); // Default address, no jumpers
 const uint8_t led = 13;
@@ -45,22 +51,33 @@ int amtMove;                  //Number of steps motor should move
 
 bool blinkState = false;      //Used to verify program active
 
+String lastcommand = "" ; //Used to keep track of commands, to avoid repeats
+unsigned long commandtime = 0;
 
 void setup()
 {
   pinMode(led, OUTPUT);
   pinMode(5, OUTPUT);           // set-up for RESET pin
+  pinMode(4, OUTPUT);            //Pin for SD logging
   AFMStop.begin();                // Start the top shield
+  
   Serial1.begin(38400);         // Ard-to-Ard serial rate
   Serial.begin(9600);           // XBee serial rate
   // AHRS uses 38400 baud
+
+//Set up GPS stuff
+  GPS.begin(9600);
+  Serial2.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  
   stepper.setMaxSpeed(5000.0);    //steps/sec, high value is limited by time
                                   //taken to call run()
   stepper.setAcceleration(200.0); //steps/sec^2
   heading = 0 * DegToSteps;       // In units of motor steps
                                   // if it does not begin with gamma at 0
   startingPosition = (Serial1.parseFloat() * DegToSteps);
-
+  
   while (!Serial){;}      //  wait for serial port to connect
 
   // initialize SD
@@ -68,13 +85,9 @@ void setup()
     Serial1.println("initialization failed!");
     return;
   }
-
  
   Serial1.println("Setup complete.");
-  
-
 }
-
 
 void loop()
 {
@@ -88,14 +101,15 @@ void loop()
       gammaPos = -Serial1.parseFloat(); 
 
       // Record position (magnetic field)
-      File myFile = SD.open("test.txt", FILE_WRITE);
+      compassfile = SD.open("test.txt", FILE_WRITE);
 
-       if(myFile){
-      myFile.print("gammaPos: "); 
-      myFile.println(gammaPos);
-      myFile.close();
+       if(compassfile){
+      compassfile.print("gammaPos: "); 
+      compassfile.println(gammaPos);
+      compassfile.close();
       }
       
+      Serial.println(gammaPos);
 
       gammaPosSteps = gammaPos * DegToSteps;  //Convert to steps
       curPos = stepper.currentPosition() - startingPosition ;  //Get current position
@@ -115,12 +129,14 @@ void loop()
       //Blink LED to show data is being received and processed
       blinkState = !blinkState; 
       digitalWrite(13, blinkState); 
+      updateGPS();
+
       stepper.run();          //Move
     }
   xBeeCommand();                              //  respond to XBee commands if any
   serialBFlush();
   stepper.run();  //Move
-  
+
   
 }
 
@@ -148,38 +164,37 @@ void xBeeCommand(){
     num = transmission.indexOf('?');
     id = transmission.substring(0, num);
     command = transmission.substring(num + 1, len);
-    
-    
-    Serial.println(transmission);
 
     //  Verify id and perform valid command //
  
 
-
      if (id == "CH") {
+
+     // if(command = lastcommand && (millis() - commandtime < 5000)) return;
       
-   //  Serial.println("CH\n");
-      Serial1.println("Neato Bandito");
-      if (command == "ZE") {
+       //  lastcommand = command;
+         //commandtime = millis();
+      
+        if (command == "ZE") {
         int mov = stepper.currentPosition()%200;
-        if( mov <-100)
-        {mov = -200-mov;}
-        else if( mov > 100)
-        {mov = 200-mov;}
-        else
-        {mov = (-1)*mov;}
-        stepper.move(mov);
-        sendToXbee("ZE RECEIVED");
-        for(int i=0; i<200; i++)
-        {
-          stepper.run();
+          if( mov <-100)
+          {mov = -200-mov;}
+          else if( mov > 100)
+          {mov = 200-mov;}
+          else
+          {mov = (-1)*mov;}
+          stepper.move(mov);
+          sendToXbee("ZE RECEIVED");
+          for(int i=0; i<200; i++)
+          {
+            stepper.run();
           }
-      }
+        }
    if (command == "RZ") {
     sendToXbee("RZ RECEIVED");
     rotSpeedScale =0;
       } 
-       if (command == "RS") {
+  if (command == "RS") {
     sendToXbee("RS RECEIVED");
     digitalWrite(5, HIGH);   // turn the RESET on (HIGH is the voltage level)
     delay(500);              // wait for a 0.5 second
@@ -193,25 +208,79 @@ void xBeeCommand(){
     sendToXbee("HE RECEIVED");
     startingPosition -=50;
       }
-    
-    
-    
-    
-    
     }
 }
 }
 
 
 void sendToXbee(String msg){
-  if(msg != "LED\n"){
-    Serial.println("CH;"+msg + "!");
-  }
-  else{
-    Serial.print(msg);
-  }
+  Serial.println("CH;"+msg + "!");
   Serial1.println("Sent " + msg + " to the xBee");
 }
+
+void updateGPS(){
+
+  
+  while (Serial2.available() > 0) {
+    GPS.read();
+  }
+
+
+  
+    if (GPS.newNMEAreceived()) {
+    int lastTime = getGPStime();
+    GPS.parse(GPS.lastNMEA());
+   
+    
+    if (getGPStime() > lastTime) {
+      gpsfile = SD.open("gpsstuff.txt", FILE_WRITE);
+
+      String data = "";
+      if (GPS.fix) {
+        data += (flightTimeStr() + "," + String(GPS.latitudeDegrees) + "," + String(GPS.longitudeDegrees) + ",");
+        data += (String(GPS.altitude * 3.28048) + ",");    //convert meters to feet for datalogging
+        data += (String(GPS.month) + "/" + String(GPS.day) + "/" + String(GPS.year) + ",");
+        data += (String(GPS.hour) + ":" + String(GPS.minute) + ":" + String(GPS.seconds) + ",");
+      }
+      else
+        data += (flightTimeStr() + ",No fix");
+     
+      
+      gpsfile.println(data);
+      gpsfile.close();
+  
+    }
+  }
+
+
+
+}
+
+
+
+int getGPStime() {    //returns GPS time as seconds since 0:00:00 UTC. Note that comparisons crossing that time will be inaccurate
+  return GPS.hour * 3600 + GPS.minute * 60 + GPS.seconds;
+}
+
+//returns milliseconds since last flight start command (or bootup)
+unsigned long flightTime() {
+  return (millis() - flightStart);
+}
+
+//returns the above flight time as a usable string for print statements
+String flightTimeStr() {
+  unsigned long t = flightTime() / 1000;
+  String fTime = "";
+  fTime += (String(t / 3600) + ":");
+  t %= 3600;
+  fTime += String(t / 600);
+  t %= 600;
+  fTime += (String(t / 60) + ":");
+  t %= 60;
+  fTime += (String(t / 10) + String(t % 10));
+  return fTime;
+}
+
 
 
 
